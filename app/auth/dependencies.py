10fc32adc3e18ea2705f01bb51a7ba0ad74f793f
FllyTMCruz <tadeobazaldua424@gmail.com>
2026-09-12 14:26:46 -0600

    Update dependencies.py

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -32,7 +32,7 @@ def get_current_user(
             token,
             settings.supabase_jwt_secret,
             algorithms=["HS256"],
-            options={"verify_aud": False}, # <--- Desactivamos la verificación estricta de aud
+            options={"verify_aud": False, "verify_iss": False},
         )
     except JWTError as exc:
         raise HTTPException(
